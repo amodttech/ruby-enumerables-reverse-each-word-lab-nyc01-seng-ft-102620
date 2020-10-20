9fe1785f335b2b4261704287d8ccf5fa76ab39a9
Aaron Amodt <aaron@amodttech.com>
2020-10-20 15:07:55 +0000

    Automatically backed up by Learn

--- reverse_each_word.rb
+++ reverse_each_word.rb
@@ -1,10 +1,9 @@
 require 'pry'
 
 string = "Hello there, and how are you?"
-new_string = []
 
 def reverse_each_word(string)
-  new_string
+  new_string = []
   new_string << string.split(/ /)
 end
 
